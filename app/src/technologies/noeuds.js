f99d0c04b448bba1c224bda1631eddd6075f98e7
thomas logeais <thomaslogeais17@gmail.com>
2025-06-12 16:17:50 +0200

    joli déblocage des technologies (toujours sans notion de coût)

--- app/src/technologies/noeuds.js
+++ app/src/technologies/noeuds.js
@@ -1,12 +1,18 @@
 
 export class Noeud {
-    constructor(idHTML, prix, typePrix, enfants = []) {
+    constructor(idHTML, prix, typePrix, menuTechonologies, enfants = []) {
         this.idHTML = idHTML;
         this.prix = prix;
         this.typePrix = typePrix;
+        this.menuTechonologies = menuTechonologies;
         this.divElementHTML = document.getElementById(idHTML);
         this.imgtechnoDebloquee = this.divElementHTML.querySelector('.debloquee');
         this.imgtechnoBloquee = this.divElementHTML.querySelector('.bloquee');
+        this.imgtechnoHiden = this.divElementHTML.querySelector('.hiden');
+        this.enfants = enfants; // Enfants du noeud, s'il y en a
+        this.divInfoNoeud = document.querySelector('#divInfoNoeud');
+
+
         this.imgtechnoBloquee.addEventListener('mousedown', (e) => {
             if(e.button  === 0){
                 this.debloquer();
@@ -14,11 +20,28 @@ export class Noeud {
                 console.log(`Clic droit sur le noeud ${this.idHTML}.`);
             }
         });
-        this.imgtechnoHiden = this.divElementHTML.querySelector('.hiden');
-        this.enfants = enfants; // Enfants du noeud, s'il y en a
+        this.imgtechnoDebloquee.addEventListener('mouseenter', () => this.detectionMouseOver());
+        this.imgtechnoBloquee.addEventListener('mouseenter', () => this.detectionMouseOver());
+
+        this.imgtechnoDebloquee.addEventListener('mouseleave', () => this.detectionMouseLeave());
+        this.imgtechnoBloquee.addEventListener('mouseleave', () => this.detectionMouseLeave());
+    }
+
+    getPosition(){
+        return {
+            x: this.divElementHTML.offsetLeft + (this.divElementHTML.offsetWidth / 2),
+            y: this.divElementHTML.offsetTop + (this.divElementHTML.offsetHeight / 2)
+        };
+    }
+    estCache() {
+        return (this.imgtechnoDebloquee.classList.contains('cachee') && this.imgtechnoBloquee.classList.contains('cachee') && this.imgtechnoHiden.classList.contains('cachee'));
+    }
+    decouvrir() {
+        this.imgtechnoHiden.classList.remove('cachee');
+        this.imgtechnoHiden.classList.add('apparition');
     }
     
-    decouvrir() {
+    decouvrirMieux() {
         // Disparition de l'image "hidden"
         this.imgtechnoHiden.classList.add('cachee');
 
@@ -26,18 +49,47 @@ export class Noeud {
         this.imgtechnoBloquee.classList.remove('cachee');
         this.imgtechnoBloquee.classList.add('apparition');
     }
-
     
     debloquer() {
         // Placeholder for unlocking the node
-        console.log(`Déblocage du noeud ${this.idHTML}.`);
         this.imgtechnoBloquee.classList.remove('apparition');
         this.imgtechnoBloquee.classList.add('cachee');
         this.imgtechnoDebloquee.classList.remove('cachee');
         this.imgtechnoDebloquee.classList.add('deblocage');
-        for(const enfant of this.enfants){
-            const noeud = new Noeud(enfant, 0, 'or');
-            noeud.decouvrir(); // Découverte de l'enfant
+        // Decouverte des enfans
+        for(const enfantId of this.enfants){
+            const noeud = this.menuTechonologies.noeuds.find(n => n.idHTML === enfantId);
+            noeud.decouvrirMieux();
+            for(const enfantDenfantId of noeud.enfants){
+                const enfantNoeud = this.menuTechonologies.noeuds.find(n => n.idHTML === enfantDenfantId);
+                enfantNoeud.decouvrir();
+            }
         }
+        this.menuTechonologies.dessinerLiensNoeuds();
+    }
+
+    detectionMouseOver() {
+        // Démarre un timer de 0.5s
+        this.timerSurvol = setTimeout(() => {
+            this.afficherInfo();
+        }, 500);
+    }
+    detectionMouseLeave(){
+        // Stoppe le timer si la souris quitte avant 0.5s
+        clearTimeout(this.timerSurvol);
+        this.cacherInfo();
+    }
+
+    afficherInfo(){
+        console.log(`Afficher`);
+        this.divInfoNoeud.style.top = `${this.divElementHTML.offsetTop - this.divInfoNoeud.offsetHeight - 35}px`;
+        this.divInfoNoeud.style.left = `${this.divElementHTML.offsetLeft - (this.divInfoNoeud.offsetWidth/2)}px`;
+        this.divInfoNoeud.classList.remove('cachee');
+        this.divInfoNoeud.classList.add('apparition');
+    }
+    cacherInfo() {
+        console.log(`Cacher`);
+        this.divInfoNoeud.classList.remove('apparition');
+        this.divInfoNoeud.classList.add('cachee');
     }
 }
--- app/src/technologies/script.js
+++ app/src/technologies/script.js
@@ -1,18 +1,63 @@
 import { Noeud } from "./noeuds.js";
 
+class MenuTechnologies {
+    constructor() {
+        this.noeuds = [
+            new Noeud("centre", 100, "or", this, ['vitesseAttaque', 'degats', 'orDeDepart', 'lvlUpTours']),
+            new Noeud("vitesseAttaque", 200, "or", this),
+            new Noeud("degats", 300, "or", this, ['critRate', 'critDamage']),
+            new Noeud("orDeDepart", 400, "or", this),
+            new Noeud("lvlUpTours", 500, "or", this),
+            new Noeud("critRate", 500, "or", this),
+            new Noeud("critDamage", 500, "or", this)
+        ];
 
-console.log("Script de gestion des technologies chargé");
-// Désactivation du menu contextuel de google Chrome
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-});
+        this.canvas = document.querySelector('#canvasTechno');
+        this.ctx = this.canvas.getContext('2d');
 
-const noeuds = [
-    new Noeud("centre", 100, "or", ['vitesseAttaque', 'degats', 'orDeDepart', 'lvlUpTours']),
-    new Noeud("vitesseAttaque", 200, "or"),
-    new Noeud("degats", 300, "or", ['critRate', 'critDamage']),
-    new Noeud("orDeDepart", 400, "or"),
-    new Noeud("lvlUpTours", 500, "or"),
-    new Noeud("critRate", 500, "or"),
-    new Noeud("critDamage", 500, "or")
-];
+
+
+
+        // Désactivation du menu contextuel de google Chrome
+        document.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+    }
+
+
+    dessinerLiensNoeuds() {
+        for(const noeud of this.noeuds) {
+            const positionNoeud = noeud.getPosition();
+
+            const posNoeudX = positionNoeud.x-noeud.divElementHTML.offsetWidth/2;
+            const posNoeudY = positionNoeud.y-noeud.divElementHTML.offsetHeight/2;
+            
+            
+            for(const enfantId of noeud.enfants) {
+                const enfant = this.noeuds.find(n => n.idHTML === enfantId);
+                if (!enfant) continue;
+                if (enfant.estCache()) continue; // Si l'enfant est caché, on ne le dessine pas
+                const positionEnfant = enfant.getPosition();
+                
+                const posEnfantX = positionEnfant.x-enfant.divElementHTML.offsetWidth/2;
+                const posEnfantY = positionEnfant.y-enfant.divElementHTML.offsetHeight/2;
+                this.ctx.beginPath();
+                this.ctx.moveTo(posEnfantX, posEnfantY);
+                this.ctx.lineTo(posNoeudX, posNoeudY);
+                this.ctx.strokeStyle = 'black';
+                this.ctx.lineWidth = 2;
+                this.ctx.stroke();
+            }
+            this.ctx.beginPath();
+            this.ctx.arc(posNoeudX, posNoeudY-2, 24, 0, Math.PI * 2);
+            this.ctx.fillStyle = 'white';
+            this.ctx.fill();
+        }
+    }
+}
+
+
+
+const menuTechnologies = new MenuTechnologies();
+menuTechnologies.dessinerLiensNoeuds();
+
